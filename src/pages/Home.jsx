db35e1110dd60720504544fdc2a0cc35da7ffd8d
fatumahomar <fatuabdi55@gmail.com>
2024-04-23 00:02:42 +0300

    altschool examination

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return(
+        <div>
+            <h1 className="title-header">GitHub API Explorer
+
+
+</h1>
+<p className="writeup">Alt-school examination.
+</p>
+        </div>
+    )
+}
+export default Home;
